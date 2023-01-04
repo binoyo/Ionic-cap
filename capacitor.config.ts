375a1833a207c32e6f9706355d5704e2070824d7
Bino <binoyohannnan@gmail.com>
2023-01-03 23:41:27 -0500

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'my-app',
+  appId: 'com.mangattu.myApp',
+  appName: 'Mangattu App',
   webDir: 'www',
   bundledWebRuntime: false
 };
